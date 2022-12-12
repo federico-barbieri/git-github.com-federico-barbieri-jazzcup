f2e211e4f53f05fe7ae4e48601e5ad568c76ea4a
Federico Barbieri <federicobarbieri54@gmail.com>
2022-12-12 17:28:26 +0100

    change hero text

--- app.js
+++ app.js
@@ -4,4 +4,37 @@ const vinyl = document.querySelector('.vinyl-hero');
 
 window.onload = () => {
     vinyl.classList.add("vinyl-spins");
+    everything();
 }
+
+
+// change name of hero h1
+
+let heroSpan = document.querySelector('.jazz-can-be');
+
+function playful(){
+    heroSpan.textContent = "playful";
+}
+
+function sad(){
+    heroSpan.textContent = "sad";
+}
+
+function dreamy(){
+    heroSpan.textContent = "dreamy";
+}
+
+function anything(){
+    heroSpan.textContent = "anything";
+}
+
+function everything () {
+    setTimeout(playful, 2000);
+    setTimeout(sad, 4000);
+    setTimeout(dreamy, 6000);
+    setTimeout(anything, 8000);
+    setTimeout(everything, 11000);
+}
+
+
+
