eff436f0a57f35e57a82aa5c251bad10f33fb42e
Federico Barbieri <federicobarbieri54@gmail.com>
2022-12-20 11:12:25 +0100

    moods

--- js/mood.js
+++ js/mood.js
@@ -31,5 +31,80 @@ const vinyl = document.querySelector('.vinyl-hero');
 
 
 window.onload = () => {
-   // vinyl.classList.add("vinyl-spins");
+    vinyl.classList.add("vinyl-spins");
 }
+
+// MOODS 
+
+const modal = document.querySelector('.modal1Content');
+
+const closeModal = document.querySelector('#closeModal1');
+
+const moodlistUl = document.querySelector('#moodlist-ul');
+
+const happy = document.querySelector('.happy');
+const sad = document.querySelector('.sad');
+const dreamy = document.querySelector('.dreamy');
+const joyful = document.querySelector('.joyful');
+const melancholic = document.querySelector('.melancholic');
+
+let modalImg = document.querySelector('.mood-img');
+
+let modalP = document.querySelector('.artist-name');
+
+// close modal
+
+closeModal.addEventListener('click', () =>{
+    modal.style.display = "none";
+    modalImg.src = "";
+})
+
+// happy modal
+
+happy.addEventListener('click', () =>{
+
+    modal.style.display = "block";
+    modalImg.src = "../img/moods/happy.jpg";
+    modalP.textContent = "Janet Leary";
+
+})
+
+// sad modal
+
+sad.addEventListener('click', () =>{
+
+    modal.style.display = "block";
+    modalImg.src = "../img/moods/sad.jpg";
+    modalP.textContent = "Oliver Hampton";
+
+})
+
+// dreamy modal
+
+dreamy.addEventListener('click', () =>{
+
+    modal.style.display = "block";
+    modalImg.src = "../img/moods/dreamy.jpg";
+    modalP.textContent = "The Power Sound";
+
+})
+
+// joyful modal
+
+joyful.addEventListener('click', () =>{
+
+    modal.style.display = "block";
+    modalImg.src = "../img/moods/joyful.jpg";
+    modalP.textContent = "Open Sounds";
+
+})
+
+// melancholic modal
+
+melancholic.addEventListener('click', () =>{
+
+    modal.style.display = "block";
+    modalImg.src = "../img/moods/melancholic.jpg";
+    modalP.textContent = "Time Travellers";
+
+})
